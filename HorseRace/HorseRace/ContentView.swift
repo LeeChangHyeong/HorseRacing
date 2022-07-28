dd24337e660a7c21a00f1a636beb99a026861a7e
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2022-07-28 21:52:05 +0900

    [ADD] 파일추가

---
+++ HorseRace/HorseRace/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HorseRace
+//
+//  Created by LeeChangHyeong on 2022/07/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HorseRace/HorseRace/HorseRaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorseRaceApp.swift
+//  HorseRace
+//
+//  Created by LeeChangHyeong on 2022/07/28.
+//
+
+import SwiftUI
+
+@main
+struct HorseRaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
