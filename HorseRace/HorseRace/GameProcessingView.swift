55274e7c08113c44d934779ae2f7b3342244778e
Dongkyu Kim <dk143496@gmail.com>
2022-08-23 11:07:23 +0900

    [FEAT] horse 제자리 움직임 애니메이션 구현

--- HorseRace/HorseRace/GameProcessingView.swift
+++ HorseRace/HorseRace/GameProcessingView.swift
@@ -17,6 +17,7 @@ struct GameProcessingView: View {
     }
 }
 
+
 class HorseRunningScene: SKScene {
     
     let backgroundStart = SKSpriteNode(imageNamed: "backgroundStart")
@@ -36,6 +37,15 @@ class HorseRunningScene: SKScene {
         backgroundMiddle.position.y = 0
         backgroundMiddle.zPosition = -10
         addChild(backgroundMiddle)
+        
+        buildHorse(color: "red")
+        animateHorse(color: "red", speed: 10)
+
+        buildHorse(color: "green")
+        animateHorse(color: "green", speed: 20)
+
+        buildHorse(color: "blue")
+        animateHorse(color: "blue", speed: 30)
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -48,5 +58,45 @@ class HorseRunningScene: SKScene {
             backgroundStart.position.x = backgroundMiddle.position.x
             backgroundMiddle.position.x += backgroundMiddle.size.width
         }
+        
+        horse.position.x += CGFloat.random(in: -5...10)
     }
+    
+    
+    func buildHorse(color: String) {
+        // horse Atlas를 생성, 여기서는 sample Red 이미지로 구성
+        // 말이 여러 마리일 경우 prameter로 말 색상 줄 수 있음 -> let horseColor: String
+        // position 변경 -> horse.position = CGPoint(x:y:) 임의로 설정해 둔 상태
+        
+        let horseAnimatedAtlas = SKTextureAtlas(named: "\(color)HorseImages")
+        var runFrames: [SKTexture] = []
+        
+        let numImages = horseAnimatedAtlas.textureNames.count
+        for i in 1...numImages {
+            let horseTextureName = "\(color)Horse\(i)"
+            runFrames.append(horseAnimatedAtlas.textureNamed(horseTextureName))
+        }
+        horseRunningFrames = runFrames
+        
+        let firstFrameTexture = horseRunningFrames[0]
+        horse = SKSpriteNode(texture: firstFrameTexture)
+        horse.position = CGPoint(x: frame.minX + 100, y: frame.midY + 100 * CGFloat.random(in: -1...1))
+        horse.size = CGSize(width: 188, height: 106)
+        addChild(horse)
+        
+    }
+    
+    
+    func animateHorse(color: String, speed: Double) {
+        // horse 애니메이션 동작, speed는 말의 다리 움직임 속도(필요한 부분인지는 잘 모르겠음)
+        let timePerFrame = 1 / speed
+        
+        horse.run(SKAction.repeatForever(
+            SKAction.animate(with: horseRunningFrames,
+                             timePerFrame: timePerFrame,
+                             resize: false,
+                             restore: true)),
+        withKey: "\(color)HorseRunning")
+    }
+    
 }
