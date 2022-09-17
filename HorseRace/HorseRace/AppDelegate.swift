8a4cff7f42f795e64b0ef12cc1c33e2f121566ef
jeohong <jeohong59@gmail.com>
2022-09-17 16:27:37 +0900

    [FIX] 런치 스크린 딜레이 수정

--- HorseRace/HorseRace/AppDelegate.swift
+++ HorseRace/HorseRace/AppDelegate.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOpions launchOptions:
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
                      [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         Thread.sleep(forTimeInterval: 2)
         return true
